44a14f93d8b31f363b7831e6cf3a25862689cf04
vaibhav <vabmh99@gmail.com>
2023-10-10 22:56:51 +0530

    created base project

---
+++ Dotnet-desk-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dotnet_desk_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
