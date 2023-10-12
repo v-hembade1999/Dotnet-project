689ff87f02580ff5989b599ea7ca34b4387616d8
vaibhav <vabmh99@gmail.com>
2023-10-12 10:27:51 +0530

    added database connection

---
+++ Dotnet-desk-project/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Dotnet_desk_project.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_desk_project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
--- Dotnet-desk-project/Program.cs
+++ Dotnet-desk-project/Program.cs
@@ -1,4 +1,8 @@
+using Dotnet_desk_project.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
